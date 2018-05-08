93d86e3e6563f7e1117a7c3147edf965371559cf
piyarat <tangpiyarat23@gmail.com>
2018-05-08 15:13:50 +0700

    Update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -4,5 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello, My name is Piyarat");
+        System.out.println("Major: Information System");
+        System.out.println("Faculty: Management Tecnology");
+        System.out.println("University: RMUTSV");
     }  //main
 }//class
